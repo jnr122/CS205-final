b573034fed114e864a83776c5dafcdb26dbfe389
jonah <jonah.n.rubin@gmail.com>
2019-10-16 22:18:34 -0400

    Dealt with player offset

--- src/Piece.java
+++ src/Piece.java
@@ -3,26 +3,28 @@ public class Piece {
     // fields
     private Area ar;
     private int playerNum;
+    private int playerOffset;
     private int loc = -1;
-    private final int PLAYEROFFSET = 7;
 
     // 28 board spaces and 4 finish spaces
-    private int BOARDSIZE = 28;
-    private int TOTALSIZE = 32;
+    private final int BOARDSIZE = 28;
+    private final int FINISHSIZE = 4;
+
+    private final int OFFSET = 7;
 
 
     // Constructor takes only player num to decide where
     Piece(int playerNum) {
         ar = Area.HOME;
         this.playerNum = playerNum;
+        playerOffset = playerNum * OFFSET;
     }
 
     // send piece to board and initialize at relative '0'
     public void toBoard() {
         ar = Area.BOARD;
         // adjust starting loc  because not all players start at 0
-        loc = playerNum * PLAYEROFFSET;
-
+        loc = playerOffset;
     }
 
     // send piece back home and reset loc to -1
@@ -35,18 +37,40 @@ public class Piece {
     public int move(int n) {
         // moving on board
         if (ar == Area.BOARD) {
-            loc = (loc + n) % TOTALSIZE;
-            // if moving into finish
-            if (loc > (BOARDSIZE + PLAYEROFFSET * playerNum)) {
-                ar = Area.FINISH;
+            // full lap
+            if (loc + n > BOARDSIZE + playerOffset) {
+                // continuing around
+                if ((loc + n) - (BOARDSIZE + playerOffset) > 4) {
+                    loc = (loc + n) % BOARDSIZE;
+                    return loc;
+                } else {
+                    // entering finish
+                    ar = Area.FINISH;
+                    loc = (loc + n) - (BOARDSIZE + playerOffset);
+                    return loc;
+                }
+
+            } else {
+                // still completing lap
+                loc += n;
+                return loc;
             }
-            return loc;
-            // if moving in finish
+            // moving in finish
+        } else if (ar == Area.FINISH) {
+            if (loc + n <= 4) {
+                loc += n;
+                return loc;
+            }
+            return -1;
         }
-//        else if (ar == Area.FINISH) {
-//
-//        }
+
+        // tried to move directly from HOME
         return -1;
+
+    }
+
+    public Area getAr() {
+        return ar;
     }
 
 
