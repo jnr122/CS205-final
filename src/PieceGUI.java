8cb356a187d0f70580be2f5ad763c8e2d73fb205
joshpace8 <55107500+joshpace8@users.noreply.github.com>
2019-12-04 19:21:19 -0500

    pieceGUI.java
    piece update

--- src/PieceGUI.java
+++ src/PieceGUI.java
@@ -1,227 +1,154 @@
-import javafx.application.Application;
-import javafx.event.EventHandler;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.StackPane;
-import javafx.scene.text.*;
-import javafx.scene.shape.*;
-import javafx.geometry.Insets;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.transform.Scale;
-import javafx.stage.Stage;
+import java.util.ArrayList;
 
-public class BoardGUI {
+/**
+ * Piece class
+ *
+ * Represents a single game piece
+ * Stores area, player num, offset, relativeLocation, board size, finish size
+ * Supports moving to board, moving to home, moving n spaces, and getting area
+ */
 
-    private GridPane board;
+public class Piece {
 
+    // fields
+    private Area ar;
+    private int playerNum;
+    private int playerOffset;
+    private int pieceNum;
+    private int relativeLoc;
+    private Type type;
 
-    public BoardGUI(){
-        board = new GridPane();
-        board.setAlignment(Pos.CENTER);
-        board.setVgap(10);
-        board.setHgap(10);
-        setSquares(board);
+    /**
+     * Constructor
+     * @param playerNum, the player's turn value
+     */
+    Piece(int playerNum, int pieceNum, Type type) {
+        ar = Area.HOME;
+        this.playerNum = playerNum;
+        this.pieceNum = pieceNum + 1;
+        this.playerOffset = playerNum * Constants.OFFSET;
+        this.toHome();
+        this.type = type;
     }
 
-    private void setSquares(GridPane board){
-        // SCALING FOR EFFECTS
-        Scale scaleUP = new Scale(1.08, 1.08);
-        scaleUP.setPivotX(75);
-        scaleUP.setPivotY(75);
-        Scale scaleDown = new Scale(1, 1);
-        scaleDown.setPivotX(0);
-        scaleDown.setPivotY(0);
+    /**
+     * Overloaded constructor to accept saved areaLoc
+     * @param playerNum, the player's turn value, and saved area and absoluteLoc
+     */
+    Piece(int playerNum, int pieceNum, Area ar, int relativeLoc, Type type) {
+        this.playerNum = playerNum;
+        this.pieceNum = pieceNum + 1;
+        this.playerOffset = playerNum * Constants.OFFSET;
+        this.ar = ar;
+        this.relativeLoc = relativeLoc;
+        this.type = type;
 
-        // The DIE
-        Label die = new Label("?");
-        board.add(die, 6,6);
-        die.setAlignment(Pos.CENTER);
-        die.getStyleClass().add("die");
-        die.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent t) {
-                die.setText("1");
+    }
+
+    /**
+     * Send piece to board and initialize at relative '0'
+     */
+    public void toBoard() {
+        ar = Area.BOARD;
+        relativeLoc = 0;
+    }
+
+    /**
+     * Send piece back home and reset relativeLoc to -1
+     */
+    public void toHome() {
+        ar = Area.HOME;
+        relativeLoc = pieceNum;
+    }
+
+
+    public void setArLoc(AreaLoc arLoc) {
+        this.ar = arLoc.ar;
+        this.relativeLoc = arLoc.loc;
+    }
+
+    /**
+     * Calculate all valid moves given a roll of n
+     * @param n
+     * @return all valid moves
+     */
+    public ArrayList<AreaLoc> getValidMoves(int n) {
+        ArrayList<AreaLoc> validMoves = new ArrayList<>();
+
+        // moving on board
+        if (ar == Area.BOARD) {
+            if (relativeLoc + n > Constants.BOARDSIZE) { // full lap
+                if ((relativeLoc + n) - (Constants.BOARDSIZE) > Constants.FINISHSIZE) { // continuing around
+                    // should only be values [1,28]
+                    validMoves.add(new AreaLoc(Area.BOARD,(((relativeLoc + n) % Constants.BOARDSIZE))));
+                } else {  // entering finish
+                    validMoves.add(new AreaLoc(Area.FINISH,(relativeLoc + n) - (Constants.BOARDSIZE)));
+                    validMoves.add(new AreaLoc(Area.BOARD,(relativeLoc + n) % Constants.BOARDSIZE));
+                }
+
+            } else {
+                validMoves.add(new AreaLoc(Area.BOARD,relativeLoc + n)); // still completing lap
             }
-        });
-        // die animation
-//        die.setOnMouseEntered(new EventHandler<MouseEvent>() {
-//            @Override
-//            public void handle(MouseEvent t) {
-//                die.getTransforms().setAll(scaleUP);
-//            }
-//        });
-//
-//        die.setOnMouseExited(new EventHandler<MouseEvent>() {
-//            @Override
-//            public void handle(MouseEvent t) {
-//                die.getTransforms().setAll(scaleDown);
-//            }
-//        });
 
+        } else if (ar == Area.FINISH) {  // moving in finish
+            if (relativeLoc + n <= Constants.FINISHSIZE) {
+                validMoves.add(new AreaLoc(Area.FINISH,relativeLoc + n));
+            }
 
-
-        //home 1 RED
-        for (int i=0; i<=3; i++) {
-            Circle boardSqaure = new Circle();
-            StackPane stacked = new StackPane();
-            String homeLabel = "HOME";
-            Label home = new Label(Character.toString(homeLabel.charAt(i)));
-            stacked.getChildren().addAll(boardSqaure, home);
-
-            boardSqaure.getStyleClass().add("redSquare");
-            boardSqaure.setCenterX(10.0f);
-            boardSqaure.setCenterY(10.0f);
-            boardSqaure.setRadius(30.0f);
-            board.add(stacked, i, 3-i);
-
-            PieceGUI piece = new PieceGUI(0);
-            board.add(piece, i, 3-i);
+        } else  {  // tried to move directly from HOME
+            if (n == Constants.FROMHOMETHRESHOLD) {
+                validMoves.add(new AreaLoc(Area.BOARD,1));
+            }
         }
 
-        //home 2 YELLOW
-        for (int i=0; i<=3; i++) {
-            Circle boardSqaure = new Circle();
-            StackPane stacked = new StackPane();
-            String homeLabel = "HOME";
-            Label home = new Label(Character.toString(homeLabel.charAt(i)));
-            stacked.getChildren().addAll(boardSqaure, home);
-
-            boardSqaure.getStyleClass().add("yellowSquare");
-            boardSqaure.setCenterX(10.0f);
-            boardSqaure.setCenterY(10.0f);
-            boardSqaure.setRadius(30.0f);
-            board.add(stacked, i, 9+i);
-
-            PieceGUI piece = new PieceGUI(1);
-            board.add(piece, i, 9+i);
-        }
-
-        //home 3 GREEN
-        for (int i=0; i<=3; i++) {
-            Circle boardSqaure = new Circle();
-            StackPane stacked = new StackPane();
-            String homeLabel = "HOME";
-            Label home = new Label(Character.toString(homeLabel.charAt(i)));
-            stacked.getChildren().addAll(boardSqaure, home);
-
-            boardSqaure.getStyleClass().add("greenSquare");
-            boardSqaure.setCenterX(10.0f);
-            boardSqaure.setCenterY(10.0f);
-            boardSqaure.setRadius(30.0f);
-            board.add(stacked, 9+i, 12-i);
-
-            PieceGUI piece = new PieceGUI(2);
-            board.add(piece, 9+i, 12-i);
-
-        }
-
-        //home 4 BLUE
-        for (int i=0; i<=3; i++) {
-            Circle boardSqaure = new Circle();
-            StackPane stacked = new StackPane();
-            String homeLabel = "HOME";
-            Label home = new Label(Character.toString(homeLabel.charAt(i)));
-            stacked.getChildren().addAll(boardSqaure, home);
-
-            boardSqaure.getStyleClass().add("blueSquare");
-            boardSqaure.setCenterX(10.0f);
-            boardSqaure.setCenterY(10.0f);
-            boardSqaure.setRadius(30.0f);
-            board.add(stacked, 9+i, i);
-
-            PieceGUI piece = new PieceGUI(3);
-            board.add(piece, 9+i, i);
-
-        }
-
-        // top row of spaces
-        for (int i=0; i<7; i++) {
-            Circle boardSqaure = new Circle();
-            boardSqaure.getStyleClass().add("boardSquare");
-            boardSqaure.setCenterX(10.0f);
-            boardSqaure.setCenterY(10.0f);
-            boardSqaure.setRadius(30.0f);
-            board.add(boardSqaure, 2, 3+i);
-        }
-
-        // bottom row of spaces
-        for (int i=0; i<7; i++) {
-            Circle boardSqaure = new Circle();
-            boardSqaure.getStyleClass().add("boardSquare");
-            boardSqaure.setCenterX(10.0f);
-            boardSqaure.setCenterY(10.0f);
-            boardSqaure.setRadius(30.0f);
-            board.add(boardSqaure, 10, 3+i);
-        }
-
-        // left column of spaces
-        for (int i=0; i<7; i++) {
-            Circle boardSqaure = new Circle();
-            boardSqaure.getStyleClass().add("boardSquare");
-            boardSqaure.setCenterX(10.0f);
-            boardSqaure.setCenterY(10.0f);
-            boardSqaure.setRadius(30.0f);
-            board.add(boardSqaure, 3+i, 2);
-        }
-
-        // right column of spaces
-        for (int i=0; i<7; i++) {
-            Circle boardSqaure = new Circle();
-            boardSqaure.getStyleClass().add("boardSquare");
-            boardSqaure.setCenterX(10.0f);
-            boardSqaure.setCenterY(10.0f);
-            boardSqaure.setRadius(30.0f);
-            board.add(boardSqaure, 3+i, 10);
-        }
-
-        //Finish 1 RED
-        for (int i=0; i<=3; i++) {
-            Circle boardSqaure = new Circle();
-            boardSqaure.getStyleClass().add("redSquare");
-            boardSqaure.setCenterX(10.0f);
-            boardSqaure.setCenterY(10.0f);
-            boardSqaure.setRadius(30.0f);
-            board.add(boardSqaure, 3, 3+i);
-        }
-
-        //finish 2 YELLOW
-        for (int i=0; i<=3; i++) {
-            Circle boardSqaure = new Circle();
-            boardSqaure.getStyleClass().add("yellowSquare");
-            boardSqaure.setCenterX(10.0f);
-            boardSqaure.setCenterY(10.0f);
-            boardSqaure.setRadius(30.0f);
-            board.add(boardSqaure, 3+i, 9);
-        }
-
-        //finish 3 GREEN
-        for (int i=0; i<=3; i++) {
-            Circle boardSqaure = new Circle();
-            boardSqaure.getStyleClass().add("greenSquare");
-            boardSqaure.setCenterX(10.0f);
-            boardSqaure.setCenterY(10.0f);
-            boardSqaure.setRadius(30.0f);
-            board.add(boardSqaure, 9, 9-i);
-        }
-
-        //finish 4 BLUE
-        for (int i=0; i<=3; i++) {
-            Circle boardSqaure = new Circle();
-            boardSqaure.getStyleClass().add("blueSquare");
-            boardSqaure.setCenterX(10.0f);
-            boardSqaure.setCenterY(10.0f);
-            boardSqaure.setRadius(30.0f);
-            board.add(boardSqaure,9-i , 3);
-        }
-
+        return validMoves;
     }
 
-    public GridPane getBoard(){
-        return this.board;
+    /**
+     * Area getter
+     * @return current area
+     */
+    public Area getAr() {
+        return ar;
+    }
+
+    /**
+     * Getters
+     */
+    /**
+     * Computes the absolute location of the piece on the board using its relative location
+     *
+     * If the piece is in HOME or FINISH absoluteLoc = relativeLoc
+     * @return the absolute location of the piece
+     */
+    public int getAbsoluteLoc() {
+        if (ar == Area.BOARD) {
+            // should only return values [1,28]
+            if (((relativeLoc + playerOffset) % Constants.BOARDSIZE) == 0) {
+                return Constants.BOARDSIZE;
+            }
+            return ((relativeLoc + playerOffset) % Constants.BOARDSIZE);
+        }
+        return relativeLoc;
+    }
+    public int getPieceNum() {
+        return pieceNum-1;
+    }
+    public int getPlayerNum() {
+        return playerNum;
+    }
+    public int getRelativeLoc() {
+        return relativeLoc;
+    }
+    public Type getType() {
+        return type;
+    }
+
+    /**
+     * Overridden toString
+     */
+    public String toString() {
+        String s = "Area: " + ar + " Rel: " + relativeLoc  + " Abs: " + getAbsoluteLoc();;
+        return s;
     }
 }
