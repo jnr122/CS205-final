6a9c412fe9a293f4527d6a40d552239e54eaf1d2
joshpace8 <55107500+joshpace8@users.noreply.github.com>
2019-12-04 19:19:40 -0500

    update piecegui.java
    finished rest of piece initialization

--- src/PieceGUI.java
+++ src/PieceGUI.java
@@ -1,53 +1,227 @@
+import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.*;
+import javafx.scene.shape.*;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.transform.Scale;
+import javafx.stage.Stage;
 
-public class PieceGUI extends Circle {
+public class BoardGUI {
 
-    public PieceGUI(int playerNum){
+    private GridPane board;
 
-        switch(playerNum){
-            case 0:
-                makeRed();
-                break;
-            case 1:
-                makeYellow();
-                break;
-            case 2:
-                makeGreen();
-                break;
-            case 3:
-                makeBlue();
-                break;
-            default:
-                break;
-        }
-        this.setCenterX(10.0f);
-        this.setCenterY(10.0f);
-        this.setRadius(25.0f);
 
-        this.setOnMouseClicked(new EventHandler<MouseEvent>() {
+    public BoardGUI(){
+        board = new GridPane();
+        board.setAlignment(Pos.CENTER);
+        board.setVgap(10);
+        board.setHgap(10);
+        setSquares(board);
+    }
+
+    private void setSquares(GridPane board){
+        // SCALING FOR EFFECTS
+        Scale scaleUP = new Scale(1.08, 1.08);
+        scaleUP.setPivotX(75);
+        scaleUP.setPivotY(75);
+        Scale scaleDown = new Scale(1, 1);
+        scaleDown.setPivotX(0);
+        scaleDown.setPivotY(0);
+
+        // The DIE
+        Label die = new Label("?");
+        board.add(die, 6,6);
+        die.setAlignment(Pos.CENTER);
+        die.getStyleClass().add("die");
+        die.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent t) {
-                System.out.println("hi");
+                die.setText("1");
             }
         });
+        // die animation
+//        die.setOnMouseEntered(new EventHandler<MouseEvent>() {
+//            @Override
+//            public void handle(MouseEvent t) {
+//                die.getTransforms().setAll(scaleUP);
+//            }
+//        });
+//
+//        die.setOnMouseExited(new EventHandler<MouseEvent>() {
+//            @Override
+//            public void handle(MouseEvent t) {
+//                die.getTransforms().setAll(scaleDown);
+//            }
+//        });
+
+
+
+        //home 1 RED
+        for (int i=0; i<=3; i++) {
+            Circle boardSqaure = new Circle();
+            StackPane stacked = new StackPane();
+            String homeLabel = "HOME";
+            Label home = new Label(Character.toString(homeLabel.charAt(i)));
+            stacked.getChildren().addAll(boardSqaure, home);
+
+            boardSqaure.getStyleClass().add("redSquare");
+            boardSqaure.setCenterX(10.0f);
+            boardSqaure.setCenterY(10.0f);
+            boardSqaure.setRadius(30.0f);
+            board.add(stacked, i, 3-i);
+
+            PieceGUI piece = new PieceGUI(0);
+            board.add(piece, i, 3-i);
+        }
+
+        //home 2 YELLOW
+        for (int i=0; i<=3; i++) {
+            Circle boardSqaure = new Circle();
+            StackPane stacked = new StackPane();
+            String homeLabel = "HOME";
+            Label home = new Label(Character.toString(homeLabel.charAt(i)));
+            stacked.getChildren().addAll(boardSqaure, home);
+
+            boardSqaure.getStyleClass().add("yellowSquare");
+            boardSqaure.setCenterX(10.0f);
+            boardSqaure.setCenterY(10.0f);
+            boardSqaure.setRadius(30.0f);
+            board.add(stacked, i, 9+i);
+
+            PieceGUI piece = new PieceGUI(1);
+            board.add(piece, i, 9+i);
+        }
+
+        //home 3 GREEN
+        for (int i=0; i<=3; i++) {
+            Circle boardSqaure = new Circle();
+            StackPane stacked = new StackPane();
+            String homeLabel = "HOME";
+            Label home = new Label(Character.toString(homeLabel.charAt(i)));
+            stacked.getChildren().addAll(boardSqaure, home);
+
+            boardSqaure.getStyleClass().add("greenSquare");
+            boardSqaure.setCenterX(10.0f);
+            boardSqaure.setCenterY(10.0f);
+            boardSqaure.setRadius(30.0f);
+            board.add(stacked, 9+i, 12-i);
+
+            PieceGUI piece = new PieceGUI(2);
+            board.add(piece, 9+i, 12-i);
+
+        }
+
+        //home 4 BLUE
+        for (int i=0; i<=3; i++) {
+            Circle boardSqaure = new Circle();
+            StackPane stacked = new StackPane();
+            String homeLabel = "HOME";
+            Label home = new Label(Character.toString(homeLabel.charAt(i)));
+            stacked.getChildren().addAll(boardSqaure, home);
+
+            boardSqaure.getStyleClass().add("blueSquare");
+            boardSqaure.setCenterX(10.0f);
+            boardSqaure.setCenterY(10.0f);
+            boardSqaure.setRadius(30.0f);
+            board.add(stacked, 9+i, i);
+
+            PieceGUI piece = new PieceGUI(3);
+            board.add(piece, 9+i, i);
+
+        }
+
+        // top row of spaces
+        for (int i=0; i<7; i++) {
+            Circle boardSqaure = new Circle();
+            boardSqaure.getStyleClass().add("boardSquare");
+            boardSqaure.setCenterX(10.0f);
+            boardSqaure.setCenterY(10.0f);
+            boardSqaure.setRadius(30.0f);
+            board.add(boardSqaure, 2, 3+i);
+        }
+
+        // bottom row of spaces
+        for (int i=0; i<7; i++) {
+            Circle boardSqaure = new Circle();
+            boardSqaure.getStyleClass().add("boardSquare");
+            boardSqaure.setCenterX(10.0f);
+            boardSqaure.setCenterY(10.0f);
+            boardSqaure.setRadius(30.0f);
+            board.add(boardSqaure, 10, 3+i);
+        }
+
+        // left column of spaces
+        for (int i=0; i<7; i++) {
+            Circle boardSqaure = new Circle();
+            boardSqaure.getStyleClass().add("boardSquare");
+            boardSqaure.setCenterX(10.0f);
+            boardSqaure.setCenterY(10.0f);
+            boardSqaure.setRadius(30.0f);
+            board.add(boardSqaure, 3+i, 2);
+        }
+
+        // right column of spaces
+        for (int i=0; i<7; i++) {
+            Circle boardSqaure = new Circle();
+            boardSqaure.getStyleClass().add("boardSquare");
+            boardSqaure.setCenterX(10.0f);
+            boardSqaure.setCenterY(10.0f);
+            boardSqaure.setRadius(30.0f);
+            board.add(boardSqaure, 3+i, 10);
+        }
+
+        //Finish 1 RED
+        for (int i=0; i<=3; i++) {
+            Circle boardSqaure = new Circle();
+            boardSqaure.getStyleClass().add("redSquare");
+            boardSqaure.setCenterX(10.0f);
+            boardSqaure.setCenterY(10.0f);
+            boardSqaure.setRadius(30.0f);
+            board.add(boardSqaure, 3, 3+i);
+        }
+
+        //finish 2 YELLOW
+        for (int i=0; i<=3; i++) {
+            Circle boardSqaure = new Circle();
+            boardSqaure.getStyleClass().add("yellowSquare");
+            boardSqaure.setCenterX(10.0f);
+            boardSqaure.setCenterY(10.0f);
+            boardSqaure.setRadius(30.0f);
+            board.add(boardSqaure, 3+i, 9);
+        }
+
+        //finish 3 GREEN
+        for (int i=0; i<=3; i++) {
+            Circle boardSqaure = new Circle();
+            boardSqaure.getStyleClass().add("greenSquare");
+            boardSqaure.setCenterX(10.0f);
+            boardSqaure.setCenterY(10.0f);
+            boardSqaure.setRadius(30.0f);
+            board.add(boardSqaure, 9, 9-i);
+        }
+
+        //finish 4 BLUE
+        for (int i=0; i<=3; i++) {
+            Circle boardSqaure = new Circle();
+            boardSqaure.getStyleClass().add("blueSquare");
+            boardSqaure.setCenterX(10.0f);
+            boardSqaure.setCenterY(10.0f);
+            boardSqaure.setRadius(30.0f);
+            board.add(boardSqaure,9-i , 3);
+        }
+
     }
 
-    private void makeRed(){
-        this.getStyleClass().add("redPiece");
-    }
-
-    private void makeYellow(){
-        this.getStyleClass().add("yellowPiece");
-    }
-
-    private void makeGreen(){
-        this.getStyleClass().add("greenPiece");
-    }
-
-    private void makeBlue(){
-        this.getStyleClass().add("bluePiece");
+    public GridPane getBoard(){
+        return this.board;
     }
 }
